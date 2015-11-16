64d5b10ad20e291f81828e7e4a02032ff40b8ba0
ForrestWeston <Forrest.Weston@gmail.com>
2015-11-15 16:28:56 -0800

    Parses files and listens for connections

--- main.go
+++ main.go
@@ -4,17 +4,32 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"path"
 )
+
+const INF = 64
+
+type routerInfo struct {
+	addr string
+	port int
+}
+
+type linkInfo struct {
+	cost   int
+	local  int
+	remote int
+}
 
 type router struct {
 	name     string
 	testdir  string
 	poisoned bool
-	table    map[string]int
-	distv    map[string]int
-	sock     int
+	table    map[string]routerInfo
+	distv    map[string]linkInfo
+	addr     string
+	port     int
 }
 
 func main() {
@@ -30,37 +45,97 @@ func main() {
 		name:     routername,
 		testdir:  testdir,
 		poisoned: *dashp,
-		table:    make(map[string]int),
-		distv:    make(map[string]int),
-		sock:     0,
+		table:    make(map[string]routerInfo),
+		distv:    make(map[string]linkInfo),
+		addr:     "localhost",
+		port:     0,
 	}
 
 	ReadConfigFiles(r)
+	DisplayConfig(r)
 
-	//CreateConnectAllSockets()
+	ListenForMsg(r)
 
 	//DistanceVector()
 }
 
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(0)
+	}
+}
+
+func ListenForMsg(r *router) error {
+	ServerAddr, err :=
+		net.ResolveUDPAddr("udp", ":"+fmt.Sprint(r.port))
+
+	CheckError(err)
+
+	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	CheckError(err)
+	defer ServerConn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := ServerConn.ReadFromUDP(buf)
+		fmt.Println("Recieved", string(buf[0:n]), " from ", addr)
+
+		if err != nil {
+			fmt.Println("ERROR:", err)
+		}
+	}
+}
+
+func DisplayConfig(r *router) error {
+	fmt.Printf("Name: %s Address: %s Port: %d\n", r.name, r.addr, r.port)
+	fmt.Println("Router table:\n", r.table)
+	fmt.Println("distance vec:\n", r.distv)
+	return nil
+}
+
 func ReadConfigFiles(r *router) error {
-	rpath := path.Join(r.testdir, r.name+".cfg")
-	fi, err := os.Open(rpath)
+	cfgpath := path.Join(r.testdir, r.name+".cfg")
+	fi, err := os.Open(cfgpath)
 	if err != nil {
 		return err
 	}
 	defer fi.Close()
 
-	var a string
-	var b, c, d int
+	var name, addr string
+	var cost, local, remote, port int
 
 	scan := bufio.NewScanner(fi)
-	//foreach line in the file
 	for scan.Scan() {
-		fmt.Sscanf(scan.Text(), "%s %d %d %d", &a, &b, &c, &d)
-		fmt.Println(a, b, c, d)
+		fmt.Sscanf(scan.Text(),
+			"%s %d %d %d", &name, &cost, &local, &remote)
+
+		li := linkInfo{
+			cost:   cost,
+			local:  local,
+			remote: remote,
+		}
+		r.distv[name] = li
 	}
 
-	//cfg, err := ioutil.ReadFile("/" + r.testdir + "/" + r.name + ".cfg")
-	//rtr, err := ioutil.ReadFile("/" + r.testdir + "/routers")
+	rtrpath := path.Join(r.testdir, "routers")
+	fi, err = os.Open(rtrpath)
+	if err != nil {
+		return err
+	}
+
+	scan = bufio.NewScanner(fi)
+	for scan.Scan() {
+		fmt.Sscanf(scan.Text(), "%s %s %d", &name, &addr, &port)
+
+		ri := routerInfo{
+			addr: addr,
+			port: port,
+		}
+		r.table[name] = ri
+	}
+	r.port = r.table[r.name].port
+
 	return nil
 }
